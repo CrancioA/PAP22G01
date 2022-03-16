724f5275c17ccfb8802e257b9ad3563392953e28
Adrian <adrian.cranciova@yahoo.ro>
2022-03-16 15:34:58 +0200

    homework1

---
+++ Homeworks/homework1.py
@@ -0,0 +1,51 @@
+"""
+1. Create decorator for the following function that will
+- add line to the opened file containing the number of times function was called
+- line format "Starting function call: "
+
+2. Create decorator for the following function that will
+- force all args and kwargs to the type specified as decorator arg
+"""
+from functools import wraps
+
+def add_line(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        f = open('file', "w")
+        f.write('Starting function call: ' + str(wrapper.calls))
+        f.close()
+        return func(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+
+# def set_type(func):
+#     def inception(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             return func(str(*args, **kwargs))
+#         return wrapper
+#     return inception
+
+def set_type(tip):
+    def inception(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*map(tip, args, **kwargs))
+        return wrapper
+    return inception
+
+@set_type(str)
+@add_line
+def open_file(name, mode='r'):
+    return open(name, mode)
+
+open_file('file.txt', "w")
+open_file('file.txt', "w")
+open_file('file.txt', "w")
+# open_file('file.txt', "w")
+
+
+
